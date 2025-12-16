3abbb54380d67b5742fcb2af1021e898cad39cf3
bala-amypo <bala@amypo.in>
2025-12-16 15:37:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentcontroller.java
+++ src/main/java/com/example/demo/controller/studentcontroller.java
@@ -4,6 +4,22 @@ public class ApiKey{
     private long id;
     @Column(unique=true);
     private String keyValue;
-    private long 
+    private long ownerId;
+    private boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public ApiKey(){}
+
+    public ApiKey(String keyValue,){
+        this.keyValue=keyValue;
+
+    }
+    public getKeyValue(){
+        return keyValue;
+    }
+    public setKeyValue(){
+        return keyValue;
+    }
 
 }
