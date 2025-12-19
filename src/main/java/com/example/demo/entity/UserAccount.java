c1fb8d28be3f214e2893c3fa0e8746def35a9298
bala-amypo <bala@amypo.in>
2025-12-19 10:36:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,5 +1,18 @@
 package com.example.demo.entity;
-
+import jakarta.persistence.*;
+@Entity
 public class UserAccount{
-    
+    @ManyToManyQuotaPlan
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+    private String quotaPlans;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId()
 }
