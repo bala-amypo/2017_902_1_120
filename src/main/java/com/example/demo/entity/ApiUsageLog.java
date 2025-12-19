3153b20d045b1e6a3b42ea445fc50904c4efd885
bala-amypo <bala@amypo.in>
2025-12-19 08:00:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiUsageLog.java
+++ src/main/java/com/example/demo/entity/ApiUsageLog.java
@@ -1,7 +1,14 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
+import java.Time.*;
+
+@Entity
 public class ApiUsageLog{
+    @ManyToOneApikey
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    
+    private Long id;
+    private String apiKey;
+    private String endpoint;
+    private Timestamp timestamp;
 }
