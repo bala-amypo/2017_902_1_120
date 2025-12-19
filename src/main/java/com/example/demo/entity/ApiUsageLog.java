78ffb9a28a46a39b91127357a3ea4385be375a62
bala-amypo <bala@amypo.in>
2025-12-19 08:03:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiUsageLog.java
+++ src/main/java/com/example/demo/entity/ApiUsageLog.java
@@ -11,4 +11,17 @@ public class ApiUsageLog{
     private String apiKey;
     private String endpoint;
     private Timestamp timestamp;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getApiKey(){
+        return apiKey;
+    }
+    public void setApiKey(String apiKey){
+        this.apiKey=apiKey;
+    }
 }
