cf06afc1e25fbd4381471763e15119dca6bfc631
bala-amypo <bala@amypo.in>
2025-12-19 10:39:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -14,5 +14,20 @@ public class UserAccount{
     public Long getId(){
         return id;
     }
-    public void setId()
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public 
 }
