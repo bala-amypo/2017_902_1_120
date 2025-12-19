ed28d82380fb8bb9f4710b9bdbd5c98b94a505e2
bala-amypo <bala@amypo.in>
2025-12-19 10:09:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/RateLimitEnforcement.java
+++ src/main/java/com/example/demo/entity/RateLimitEnforcement.java
@@ -2,9 +2,16 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 @Entity
 public class RateLimitEnforcement{
+    @ManyToOneApiKey
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String apiKey;
-    private String end
+    private String endpoint;
+    private Timestamp timestamp;
+
+    public Long getId(){
+        return id;
+    }
+    public 
 }
