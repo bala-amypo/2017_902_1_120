b8fc0824ceed7ae852b43d3cb73d4de6a305a33b
bala-amypo <bala@amypo.in>
2025-12-20 17:01:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
+++ src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
@@ -7,3 +7,11 @@
     
 // }
 
+package com.example.demo.repository;
+
+import com.example.demo.entity.RateLimitEnforcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RateLimitEnforcementRepository
+        extends JpaRepository<RateLimitEnforcement, Long> {
+}
--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -11,10 +11,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-@Repository
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    Optional<UserAccount> findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserAccount, Long> {
+
+    Optional<UserAccount> findByUsername(String username);
 }
+
+
