977ece7681d12481f25c957074763d9a63090ec0
bala-amypo <bala@amypo.in>
2025-12-20 03:50:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/KeyExemptionRepository.java
+++ src/main/java/com/example/demo/repository/KeyExemptionRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
-
-public interface keyExemptionRepository{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.KeyExemptionRepository;
+@repository
+public interface keyExemptionRepository extends JpaRepository<keyExemptionRepository,Integer>{
+    
     
 }
--- src/main/java/com/example/demo/repository/QuotaPlanRepository.java
+++ src/main/java/com/example/demo/repository/QuotaPlanRepository.java
@@ -1,4 +1,4 @@
 package com.example.demo.repository;
-public interface QuotaPlanRepository{
+public interface QuotaPlanRepository extends JpaRepository<QuotaP{
     
 }
