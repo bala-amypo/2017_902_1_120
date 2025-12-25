b68f1cdb8a1af21d651d3f91f6282353d35e99e2
bala-amypo <bala@amypo.in>
2025-12-25 15:44:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
+++ src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
@@ -1,26 +1,11 @@
-// package com.example.demo.repository;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import com.example.demo.entity.RateLimitEnforcementRepository;
-// @Repository
-// public interface RateLimitEnforcementRepository extends JpaRepository<RateLimitEnforcementRepository,Integer>{
-    
-// }
-
-// package com.example.demo.repository;
-
-// import com.example.demo.entity.RateLimitEnforcement;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface RateLimitEnforcementRepository
-//         extends JpaRepository<RateLimitEnforcement, Long> {
-// }
-
 package com.example.demo.repository;
 
 import com.example.demo.entity.RateLimitEnforcement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RateLimitEnforcementRepository
         extends JpaRepository<RateLimitEnforcement, Long> {
 }
+
