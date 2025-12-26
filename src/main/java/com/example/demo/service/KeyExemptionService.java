dfbbb390eecab04c6dd01f91ee088a41501141ae
bala-amypo <bala@amypo.in>
2025-12-26 03:28:14 +0000

    User commit

--- src/main/java/com/example/demo/service/KeyExemptionService.java
+++ src/main/java/com/example/demo/service/KeyExemptionService.java
@@ -1,14 +1,3 @@
-// package com.example.demo.service;
-
-// import com.example.demo.entity.KeyExemption;
-// import java.util.List;
-
-// public interface KeyExemptionService {
-
-//     List<KeyExemption> getAllExemptions();
-
-//     KeyExemption create(KeyExemption exemption);
-//  }
 package com.example.demo.service;
 
 import com.example.demo.entity.KeyExemption;
