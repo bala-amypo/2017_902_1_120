8223bae3d3e9728d54622490d5f99e821520b633
bala-amypo <bala@amypo.in>
2025-12-26 03:25:14 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/KeyExemptionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/KeyExemptionServiceImpl.java
@@ -1,55 +1,3 @@
-// // package com.example.demo.service.impl;
-
-// // import com.example.demo.entity.KeyExemption;
-// // import com.example.demo.repository.KeyExemptionRepository;
-// // import com.example.demo.service.KeyExemptionService;
-// // import org.springframework.stereotype.Service;
-
-// // import java.util.List;
-
-// // @Service
-// // public class KeyExemptionServiceImpl implements KeyExemptionService {
-
-// //     private final KeyExemptionRepository repository;
-
-// //     public KeyExemptionServiceImpl(KeyExemptionRepository repository) {
-// //         this.repository = repository;
-// //     }
-
-// //     @Override
-// //     public List<KeyExemption> getAllExemptions() {
-// //         return repository.findAll();
-// //     }
-// // }
-
-// package com.example.demo.service.impl;
-
-// import com.example.demo.entity.KeyExemption;
-// import com.example.demo.repository.KeyExemptionRepository;
-// import com.example.demo.service.KeyExemptionService;
-// import org.springframework.stereotype.Service;
-
-// import java.util.List;
-
-// @Service
-// public class KeyExemptionServiceImpl implements KeyExemptionService {
-
-//     private final KeyExemptionRepository keyExemptionRepository;
-
-//     public KeyExemptionServiceImpl(KeyExemptionRepository keyExemptionRepository) {
-//         this.keyExemptionRepository = keyExemptionRepository;
-//     }
-
-//     @Override
-//     public List<KeyExemption> getAllExemptions() {
-//         return keyExemptionRepository.findAll();
-//     }
-
-//     @Override
-//     public KeyExemption create(KeyExemption exemption) {
-//         return keyExemptionRepository.save(exemption);
-//     }
-//   }
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.KeyExemption;
@@ -62,21 +10,19 @@ import java.util.List;
 @Service
 public class KeyExemptionServiceImpl implements KeyExemptionService {
 
-    private final KeyExemptionRepository keyExemptionRepository;
+    private final KeyExemptionRepository repository;
 
-    public KeyExemptionServiceImpl(KeyExemptionRepository keyExemptionRepository) {
-        this.keyExemptionRepository = keyExemptionRepository;
+    public KeyExemptionServiceImpl(KeyExemptionRepository repository) {
+        this.repository = repository;
     }
 
     @Override
     public List<KeyExemption> getAllExemptions() {
-        return keyExemptionRepository.findAll();
+        return repository.findAll();
     }
 
     @Override
     public KeyExemption create(KeyExemption exemption) {
-        return keyExemptionRepository.save(exemption);
+        return repository.save(exemption);
     }
 }
-
-
