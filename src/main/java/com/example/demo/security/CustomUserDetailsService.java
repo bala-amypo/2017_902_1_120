1889e7bde2084454c61dd34adc9d7916399914c7
bala-amypo <bala@amypo.in>
2025-12-27 10:01:11 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,34 +1,3 @@
-// package com.example.demo.security;
-
-// import com.example.demo.entity.UserAccount;
-// import com.example.demo.repository.UserAccountRepository;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
-
-// @Service   
-// public class CustomUserDetailsService implements UserDetailsService {
-
-//     private final UserAccountRepository userRepo;
-
-//     public CustomUserDetailsService(UserAccountRepository userRepo) {
-//         this.userRepo = userRepo;
-//     }
-
-//     @Override
-//     public UserDetails loadUserByUsername(String email) {
-//         UserAccount user = userRepo.findByEmail(email)
-//                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-//         return org.springframework.security.core.userdetails.User
-//                 .withUsername(user.getEmail())
-//                 .password(user.getPassword())
-//                 .roles(user.getRole().replace("ROLE_", ""))
-//                 .build();
-//     }
-// 
-
 package com.example.demo.security;
 
 import com.example.demo.entity.UserAccount;
