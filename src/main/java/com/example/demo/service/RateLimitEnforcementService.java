92d2f04534cb29e280a98a4c21a43043a5574d8f
bala-amypo <bala@amypo.in>
2025-12-28 13:00:24 +0000

    User commit

--- src/main/java/com/example/demo/service/RateLimitEnforcementService.java
+++ src/main/java/com/example/demo/service/RateLimitEnforcementService.java
@@ -9,3 +9,13 @@
 //     List<RateLimitEnforcement> getEnforcementsForKey(Long keyId);
 // }
 
+package com.example.demo.service;
+
+import com.example.demo.entity.RateLimitEnforcement;
+import java.util.List;
+
+public interface RateLimitEnforcementService {
+    RateLimitEnforcement createEnforcement(RateLimitEnforcement enforcement);
+    RateLimitEnforcement getEnforcementById(Long id);
+    List<RateLimitEnforcement> getEnforcementsForKey(Long keyId);
+}
